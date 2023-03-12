ec88c281e9efc4806e58b9069e4cae4be0e691ca
Hossein <h.dvd13@gmail.com>
2023-03-12 16:23:37 -0700

    lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+}
+
+function appendCat(name) {
+    return [...cats, name]
+}
+
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+function removeLastCat(name) {
+    const newCats = [...cats];
+    newCats.pop();
+    return newCats;
+}
+
+function removeFirstCat(name) {
+    const newCats = [...cats];
+    newCats.shift();
+    return newCats;
+}
